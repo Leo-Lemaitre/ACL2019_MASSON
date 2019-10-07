7abdcbb988dd3c44e3c6776a5ccafc7e46346bf5
lemaitr24u <lemaitr24u@ensem-204gpc08.ad.univ-lorraine.fr>
2019-10-07 11:37:33 +0200

    Creating the class Pacman with a constructor and 2 attributes

--- src/Model/Pacman.java
+++ src/Model/Pacman.java
@@ -2,4 +2,29 @@ package Model;
 
 public class Pacman {
 
+	private int posX;
+	private int posY;
+	
+	public Pacman() {
+		this.posX=5;
+		this.posY=5;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+	
 }
