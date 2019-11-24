2a378fc4e1f0cd25fe6a22c7048dae0fbbec112f
Oussama Moutiq <oussama.moutiq@gmail.com>
2019-11-24 23:19:20 +0100

    "Labyrinthe"_beginning

--- src/Generator.java
+++ src/Generator.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Generator {
-
+	public static void labyrinthe() {
+		String fichier = "labyrinthe.txt";
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(fichier));
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(sc.hasNextLine()) {
+			String uneligne = sc.nextLine();
+			Scanner scLigne = new Scanner(uneligne);
+			System.out.println(scLigne.hasNext());
+			}
+	}
 	
 }
