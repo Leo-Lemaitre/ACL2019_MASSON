f9ba21ddcf282cb309f0ca1de50befdb7f6bbaa9
masson242u <masson242u@ensem-305vpc06.ad.univ-lorraine.fr>
2019-11-25 12:15:31 +0100

    creation and display of lab initialize first position of heros

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.Vector;
-
 public class Main {
 
 	public static void main(String[] args) {
 			Vector<String> lab = Generator.labyrinthe();
-			System.out.println(lab);
-			//Generator.affichage_labyrinthe();
+			//System.out.println(lab);
+			Generator.affichage_labyrinthe(lab);
+			
+			
+			
+			
 	}
-
 }
